835c3d2b801999c1e9cc32d2b1125a995a98c301
Émerson Conde <emersonsilvacaucaia27@gmail.com>
2025-01-26 13:49:50 +0000

    Logo Correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import academyLogo from '/academy-img.png'
 import './App.css'
-
 function Menu(){
   return (
     <>
     <header>
       <div id='logo'>
-        <img src="/src/assets/academy-img.png" alt="Academy Logo" />
+        <img src={academyLogo} alt="Academy Logo" />
         <h1>ACADEMY</h1>
       </div>
       <nav>
